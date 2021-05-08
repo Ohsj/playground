2c658416110a06b78a58b5fd3a2473a86228467c
ohsj <zasqw456@naver.com>
2021-05-08 16:25:21 +0900

    basecontroller, baseservice add and logger modify

--- src/main/java/com/osj4532/playground/service/BaseService.java
+++ src/main/java/com/osj4532/playground/service/BaseService.java
@@ -1,8 +1,9 @@
-package com.osj4532.playground.controller;
+package com.osj4532.playground.service;
+
 
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class BaseController {
+public abstract class BaseService {
     protected final Logger logger = (Logger) LoggerFactory.getLogger(getClass());
 }
