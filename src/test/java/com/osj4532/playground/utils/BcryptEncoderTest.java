db4ebd62895501ed72985750177875066ba977a1
ohsj <zasqw456@naver.com>
2021-06-19 18:52:34 +0900

    Encoder created and hashing study

---
+++ src/test/java/com/osj4532/playground/utils/BcryptEncoderTest.java
@@ -0,0 +1,29 @@
+package com.osj4532.playground.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class BcryptEncoderTest {
+    @InjectMocks
+    BcryptEncoder encoder;
+
+    @Test
+    void encoderTest() {
+        String password = "qweqweqwe";
+        String result = encoder.encode(password);
+
+        boolean isMatch = encoder.matches(password, result);
+
+        assertThat(result).isNotEmpty();
+        assertThat(isMatch).isTrue();
+
+        System.out.println(result);
+
+    }
+}
+
